72bad82b133504b3d8dec66b35bf98b8e9eb3798
Dibya <dibya.suvedi@penahak.com>
2026-03-20 10:59:26 +0545

    data fixed

--- api/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519b83ba157605c1ffc408a404ca137f923d7630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b98c7eba00ee42478b0419a31b645914e4c6a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
